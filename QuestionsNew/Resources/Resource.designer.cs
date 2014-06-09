049ba5a07d26b72b57e9794ed76629cb1ca76287
samthecarman <shmuelspanier@hotmail.com>
2014-06-09 13:28:27 -0400

    Started applying the design

--- QuestionsNew/Resources/Resource.designer.cs
+++ QuestionsNew/Resources/Resource.designer.cs
@@ -42,6 +42,25 @@ namespace QuestionsNewAndroid
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int action_theme_color = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int custom_theme_color = 2131034112;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
@@ -72,6 +91,9 @@ namespace QuestionsNewAndroid
 			// aapt resource value: 0x7f020008
 			public const int Icon_question = 2130837512;
 			
+			// aapt resource value: 0x7f020009
+			public const int questionButton = 2130837513;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -85,131 +107,134 @@ namespace QuestionsNewAndroid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060021
-			public const int ActionModeAnswerItem = 2131099681;
+			// aapt resource value: 0x7f080022
+			public const int ActionModeAnswerItem = 2131230754;
 			
-			// aapt resource value: 0x7f060024
-			public const int ActionModeDeleteItem = 2131099684;
+			// aapt resource value: 0x7f080025
+			public const int ActionModeDeleteItem = 2131230757;
 			
-			// aapt resource value: 0x7f060022
-			public const int ActionModeEditItem = 2131099682;
+			// aapt resource value: 0x7f080023
+			public const int ActionModeEditItem = 2131230755;
 			
-			// aapt resource value: 0x7f060023
-			public const int ActionModeViewItem = 2131099683;
+			// aapt resource value: 0x7f080024
+			public const int ActionModeViewItem = 2131230756;
 			
-			// aapt resource value: 0x7f060025
-			public const int action_help = 2131099685;
+			// aapt resource value: 0x7f080026
+			public const int action_help = 2131230758;
 			
-			// aapt resource value: 0x7f060011
-			public const int addQuestion = 2131099665;
+			// aapt resource value: 0x7f080011
+			public const int addQuestion = 2131230737;
 			
-			// aapt resource value: 0x7f060026
-			public const int answerQuestions = 2131099686;
+			// aapt resource value: 0x7f080027
+			public const int answerQuestions = 2131230759;
 			
-			// aapt resource value: 0x7f060019
-			public const int answerText = 2131099673;
+			// aapt resource value: 0x7f08001a
+			public const int answerText = 2131230746;
 			
-			// aapt resource value: 0x7f06001d
-			public const int answersView = 2131099677;
+			// aapt resource value: 0x7f08001e
+			public const int answersView = 2131230750;
 			
-			// aapt resource value: 0x7f060003
-			public const int cancelAnswersButton = 2131099651;
+			// aapt resource value: 0x7f080003
+			public const int cancelAnswersButton = 2131230723;
 			
-			// aapt resource value: 0x7f06000a
-			public const int cancelButton = 2131099658;
+			// aapt resource value: 0x7f08000a
+			public const int cancelButton = 2131230730;
 			
-			// aapt resource value: 0x7f06001a
-			public const int dateAnswered = 2131099674;
+			// aapt resource value: 0x7f08001b
+			public const int dateAnswered = 2131230747;
 			
-			// aapt resource value: 0x7f060029
-			public const int deleteGroup = 2131099689;
+			// aapt resource value: 0x7f08002a
+			public const int deleteGroup = 2131230762;
 			
-			// aapt resource value: 0x7f060027
-			public const int editAddQuestions = 2131099687;
+			// aapt resource value: 0x7f080028
+			public const int editAddQuestions = 2131230760;
 			
-			// aapt resource value: 0x7f060008
-			public const int editAnswer = 2131099656;
+			// aapt resource value: 0x7f080008
+			public const int editAnswer = 2131230728;
 			
-			// aapt resource value: 0x7f06000e
-			public const int editGroupName = 2131099662;
+			// aapt resource value: 0x7f08000e
+			public const int editGroupName = 2131230734;
 			
-			// aapt resource value: 0x7f060018
-			public const int editQuestion = 2131099672;
+			// aapt resource value: 0x7f080019
+			public const int editQuestion = 2131230745;
 			
-			// aapt resource value: 0x7f060016
-			public const int groupListView = 2131099670;
+			// aapt resource value: 0x7f080016
+			public const int groupListView = 2131230742;
 			
-			// aapt resource value: 0x7f06001c
-			public const int groupName = 2131099676;
+			// aapt resource value: 0x7f08001d
+			public const int groupName = 2131230749;
 			
-			// aapt resource value: 0x7f06001b
-			public const int imageInfo = 2131099675;
+			// aapt resource value: 0x7f08001c
+			public const int imageInfo = 2131230748;
 			
-			// aapt resource value: 0x7f060013
-			public const int linearClick = 2131099667;
+			// aapt resource value: 0x7f080013
+			public const int linearClick = 2131230739;
 			
-			// aapt resource value: 0x7f06000b
-			public const int linearLayout1 = 2131099659;
+			// aapt resource value: 0x7f08000b
+			public const int linearLayout1 = 2131230731;
 			
-			// aapt resource value: 0x7f06000d
-			public const int linearLayout2 = 2131099661;
+			// aapt resource value: 0x7f08000d
+			public const int linearLayout2 = 2131230733;
 			
-			// aapt resource value: 0x7f060005
-			public const int linearLayout3 = 2131099653;
+			// aapt resource value: 0x7f080005
+			public const int linearLayout3 = 2131230725;
 			
-			// aapt resource value: 0x7f060007
-			public const int linearLayout4 = 2131099655;
+			// aapt resource value: 0x7f080007
+			public const int linearLayout4 = 2131230727;
 			
-			// aapt resource value: 0x7f060015
-			public const int newGroupButton = 2131099669;
+			// aapt resource value: 0x7f080015
+			public const int newGroupButton = 2131230741;
 			
-			// aapt resource value: 0x7f060000
-			public const int questionGroupName = 2131099648;
+			// aapt resource value: 0x7f080000
+			public const int questionGroupName = 2131230720;
 			
-			// aapt resource value: 0x7f060001
-			public const int questionList = 2131099649;
+			// aapt resource value: 0x7f080001
+			public const int questionList = 2131230721;
 			
-			// aapt resource value: 0x7f060012
-			public const int questionListView = 2131099666;
+			// aapt resource value: 0x7f080012
+			public const int questionListView = 2131230738;
 			
-			// aapt resource value: 0x7f060006
-			public const int questionText = 2131099654;
+			// aapt resource value: 0x7f080006
+			public const int questionText = 2131230726;
 			
-			// aapt resource value: 0x7f060004
-			public const int relativeLayout1 = 2131099652;
+			// aapt resource value: 0x7f080004
+			public const int relativeLayout1 = 2131230724;
 			
-			// aapt resource value: 0x7f060002
-			public const int saveAnswersButton = 2131099650;
+			// aapt resource value: 0x7f080002
+			public const int saveAnswersButton = 2131230722;
 			
-			// aapt resource value: 0x7f06000f
-			public const int saveGroupName = 2131099663;
+			// aapt resource value: 0x7f08000f
+			public const int saveGroupName = 2131230735;
 			
-			// aapt resource value: 0x7f060009
-			public const int saveQuestionsButton = 2131099657;
+			// aapt resource value: 0x7f080009
+			public const int saveQuestionsButton = 2131230729;
 			
-			// aapt resource value: 0x7f060010
-			public const int setReminder = 2131099664;
+			// aapt resource value: 0x7f080010
+			public const int setReminder = 2131230736;
 			
-			// aapt resource value: 0x7f060020
-			public const int specificAnswerList = 2131099680;
+			// aapt resource value: 0x7f080021
+			public const int specificAnswerList = 2131230753;
 			
-			// aapt resource value: 0x7f06001f
-			public const int textAnswerDate = 2131099679;
+			// aapt resource value: 0x7f080020
+			public const int textAnswerDate = 2131230752;
 			
-			// aapt resource value: 0x7f060014
-			public const int textGroupName = 2131099668;
+			// aapt resource value: 0x7f080014
+			public const int textGroupName = 2131230740;
 			
-			// aapt resource value: 0x7f06001e
-			public const int textQuestionGroup = 2131099678;
+			// aapt resource value: 0x7f08001f
+			public const int textQuestionGroup = 2131230751;
 			
-			// aapt resource value: 0x7f06000c
-			public const int textView1 = 2131099660;
+			// aapt resource value: 0x7f08000c
+			public const int textView1 = 2131230732;
 			
-			// aapt resource value: 0x7f060017
-			public const int textView2 = 2131099671;
+			// aapt resource value: 0x7f080018
+			public const int textView2 = 2131230744;
 			
-			// aapt resource value: 0x7f060028
-			public const int viewAnswers = 2131099688;
+			// aapt resource value: 0x7f080017
+			public const int textViewExisting = 2131230743;
+			
+			// aapt resource value: 0x7f080029
+			public const int viewAnswers = 2131230761;
 			
 			static Id()
 			{
@@ -273,14 +298,14 @@ namespace QuestionsNewAndroid
 		public partial class Menu
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int actionModeMain = 2131034112;
+			// aapt resource value: 0x7f070000
+			public const int actionModeMain = 2131165184;
 			
-			// aapt resource value: 0x7f050001
-			public const int mainAction = 2131034113;
+			// aapt resource value: 0x7f070001
+			public const int mainAction = 2131165185;
 			
-			// aapt resource value: 0x7f050002
-			public const int questionGroupMenu = 2131034114;
+			// aapt resource value: 0x7f070002
+			public const int questionGroupMenu = 2131165186;
 			
 			static Menu()
 			{
@@ -325,6 +350,34 @@ namespace QuestionsNewAndroid
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060003
+			public const int LayoutWithPadding = 2131099651;
+			
+			// aapt resource value: 0x7f060001
+			public const int MyActionBar = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int MyDefaultButtons = 2131099650;
+			
+			// aapt resource value: 0x7f060000
+			public const int MyTheme = 2131099648;
+			
+			// aapt resource value: 0x7f060004
+			public const int TextViewHeaders = 2131099652;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
